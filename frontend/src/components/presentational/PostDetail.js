af59985dcfa6573323a15ae0437296bc3fed4a19
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-10-04 23:51:54 -0300

    Adding PostDetail for the modal in order to show the Post Detail Information

---
+++ frontend/src/components/presentational/PostDetail.js
@@ -0,0 +1,13 @@
+import React from  'react';
+
+const PostDetail = (props) => {
+    return(
+        <div className="post">
+            <div className="post-top">
+
+            </div>
+        </div>
+    )
+}
+
+export default PostDetail;
