d7d8f64cfbcfb0adb74b1830832eb5a169534913
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-10-04 23:50:10 -0300

    adding the initial submit for the comments

---
+++ frontend/src/components/container/Comments.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Category from '../presentational/Post'
+
+class Comments extends Component {
+
+
+    render(){
+
+        return(
+            <div className="list-comments">
+
+
+            </div>
+        )
+    }
+}
+
+export default Comments;
---
+++ frontend/src/components/presentational/Comment.js
@@ -0,0 +1,13 @@
+import React from  'react';
+
+const Comment = (props) => {
+    return(
+        <div className="comment">
+            <div className="comment-top">
+
+            </div>
+        </div>
+    )
+}
+
+export default Comment;
