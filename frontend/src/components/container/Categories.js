69e22f01754ac9b36c4226f7185bfc43c5ca4ff0
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-10-04 23:44:14 -0300

    initial commit for the container components

---
+++ frontend/src/components/container/Categories.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Category from '../presentational/Category'
+
+class Categories extends Component {
+
+
+    render(){
+
+        return(
+            <div className="list-categories">
+
+
+            </div>
+        )
+    }
+}
+
+export default Categories;
---
+++ frontend/src/components/container/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Category from '../presentational/Post'
+
+class Main extends Component {
+
+
+    render(){
+
+        return(
+            <div className="list-posts">
+
+
+            </div>
+        )
+    }
+}
+
+export default Main;
